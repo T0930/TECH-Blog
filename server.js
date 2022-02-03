9655f6b3a2c0253bbff36d111380e2801f9dac0a
Tom M <T0930@protonmail.com>
2022-02-03 09:58:22 -0700

    added hash password to update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'SeCrEt',
+    secret: 'SECRET',
     cookie: {},
     resave: false,
     saveUninitialized: true,
